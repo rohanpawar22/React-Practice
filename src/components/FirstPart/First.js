a53c3ab8d18647339538fc14f74ce8473d391a1e
Rohan Pawar <rohan22pawar@gmail.com>
2022-05-28 12:23:40 +0530

    Created Two Components first and second divided those by diff colors

---
+++ src/components/FirstPart/First.js
@@ -0,0 +1,11 @@
+import classes from './First.module.css';
+
+const First = () => {
+    return(
+        <div className={classes.bgfirst}>
+            <h1 className={classes.text}>This is First Part</h1>
+        </div>
+    );
+}
+
+export default First;
---
+++ src/components/SecondPart/Second.js
@@ -0,0 +1,11 @@
+import classes from './Second.module.css';
+
+const Second = () => {
+    return(
+        <div className={classes.bgsecond}>
+            <h1 className={classes.text}>This is Second Part</h1>
+        </div>
+    );
+}
+
+export default Second;
